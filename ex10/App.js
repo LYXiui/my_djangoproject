6bceb3190de6fb7d794e7db3f9c6049295061f29
LYXiui <93933547+LYXiui@users.noreply.github.com>
2026-04-29 16:00:11 +0800

    Add files via upload

---
+++ ex10/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const changeText = (event) => {
+  console.log(event.target)
+  event.target.innerText = event.target.innerText + "被點了";
+};
+
+const styleArgument = { fontSize: '100px', color: 'red' };
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={styleArgument} onClick={changeText}>
+        hello CGU!!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
